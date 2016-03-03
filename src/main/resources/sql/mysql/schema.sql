bdae6d863290e6cd367a54a1c5a65ce544cd9ee5
zhangbilly <812193195@qq.com>
2016-03-03 21:33:55 +0800

    spring-side初始项目

---
+++ src/main/resources/sql/mysql/schema.sql
@@ -0,0 +1,21 @@
+drop table if exists pubtask;
+drop table if exists pubuser;
+
+create table pubtask (
+	id bigint auto_increment,
+	title varchar(128) not null,
+	description varchar(255),
+	user_id bigint not null,
+    primary key (id)
+) engine=InnoDB;
+
+create table pubuser (
+	id bigint auto_increment,
+	login_name varchar(64) not null unique,
+	name varchar(64) not null,
+	password varchar(255) not null,
+	salt varchar(64) not null,
+	roles varchar(255) not null,
+	register_date timestamp not null default 0,
+	primary key (id)
+) engine=InnoDB;
---
+++ src/main/resources/sql/oracle/schema.sql
@@ -0,0 +1,25 @@
+drop table pubtask;
+drop table pubuser;
+
+create table pubtask (
+	id number(19,0),
+	title varchar2(128) not null,
+	description varchar2(255),
+	user_id number(19,0) not null,
+	primary key (id)
+);
+
+create table pubuser (
+	id number(19,0),
+	login_name varchar2(64) not null unique,
+	name varchar2(64) not null,
+	password varchar2(255) not null,
+	salt varchar2(64) not null,
+	roles varchar2(255) not null,
+	register_date date not null,
+	primary key (id)
+);
+
+
+create sequence pubseq_task start with 100 increment by 20;
+create sequence pubseq_user start with 100 increment by 20;
