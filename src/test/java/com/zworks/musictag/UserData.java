6f7ce9e8ce1840c1b5f55d1a6f87c96b24358b55
zhangbilly <812193195@qq.com>
2016-03-08 10:38:47 +0800

    注册的测试类

---
+++ src/test/java/com/zworks/musictag/UserData.java
@@ -0,0 +1,15 @@
+package com.zworks.musictag;
+
+import org.springside.modules.test.data.RandomData;
+
+import com.zworks.musictag.entity.User;
+
+public class UserData {
+	public static User randomNewUser() {
+		User user = new User();
+		user.setLoginName(RandomData.randomName("user"));
+		user.setUserName(RandomData.randomName("UserName"));
+		user.setPlainPassword(RandomData.randomName("password"));
+		return user;
+	}
+}
---
+++ src/test/java/com/zworks/musictag/service/AccountServiceTest.java
@@ -0,0 +1,28 @@
+package com.zworks.musictag.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springside.modules.test.spring.SpringTransactionalTestCase;
+import static org.assertj.core.api.Assertions.*;
+import com.zworks.musictag.UserData;
+import com.zworks.musictag.entity.User;
+
+@TransactionConfiguration(defaultRollback=false)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
+public class AccountServiceTest extends SpringTransactionalTestCase{
+	@Autowired
+	private AccountService service;
+	
+	@Test
+	public void registerUser(){		
+		User user = UserData.randomNewUser();
+		service.registerUser(user);
+		assertThat(user.getRegisterDate()).isNotNull();
+		assertThat(user.getPassword()).isNotNull();
+		assertThat(user.getSalt()).isNotNull();
+		
+	}
+}
+
