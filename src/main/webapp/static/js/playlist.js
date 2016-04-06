23cedb5f2249911dc82dba5af570444bd0a4c611
zhangbilly <812193195@qq.com>
2016-04-06 19:13:13 +0800

    跳转歌单详情

--- src/main/webapp/static/js/playlist.js
+++ src/main/webapp/static/js/playlist.js
@@ -1,7 +1,11 @@
-MusicTag.controller("PlayListController", function($scope, songListService) {
+MusicTag.controller("PlayListController", function($scope, songListService,$state) {
 	songListService.getSongLists("1", "12").success(function(data) {
 		if (data.status == 1) {
 			$scope.songlists = data.songlists.content;
 		}
 	});
+	$scope.showSongListDetail = function(songlist){
+		var data = {"songlistid":songlist.id};
+		$state.go("songlist.songlistdetail",data);
+	}
 })
